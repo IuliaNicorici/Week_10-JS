1578844a07d60f3afee6f2ca66f51573d57ecdae
IuliaNicorici <scorobogatco@gmail.com>
2023-05-28 21:49:52 +0300

    week-10-JS

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ message();*/
 
 //Задание 2
 
-const img = document.getElementById('img');
+/*const img = document.getElementById('img');
 
 function addPrevImg(){
     console.log('addPrevImg');
@@ -17,4 +17,24 @@ function addPrevImg(){
 function addNextImg(){
     console.log('addNextImg');
     img.src = './image/2.jpg'
+}*/
+
+//задание под *
+let year = 2023;
+function isLeapYears(year) {
+    if(year % 4 ===0){
+    if(year % 100 ===0){
+    if(year % 400 ===0){
+    return true;
+    }else{
+        return false;
+    }
+    }else{
+        return true;
+    }
+    }else{
+        return false;
+    }
 }
+
+isLeapYears();
